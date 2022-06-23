d7bfb489060ab8414011f08e0e42ca1026e5447b
mubeenhassan <mubeenhassan34@gmail.com>
2022-06-24 00:21:22 +0500

    update about page

--- component/pages/about.jsx
+++ component/pages/about.jsx
@@ -6,36 +6,40 @@ const About = () => {
     return (
         <div>
             <div className={style.about_title}>
-                <h4>Beacon Business Advisors uses a strong business model with creative thinking, open and honest conversations and self observation.</h4>
+                <h1>Linger Coaching Workshops</h1>
+                <h4>Linger Coaching is a local Pacific Northwest behavior coaching business. The owners are aspired businesswomen implementing behavior changes in their clients through workshops. They believe that proper behavior coaching is essential to every individual&#39;s day-to-day life, whether at workplace, school, or even at home. In addition, they have learned that the cultivation of mindfulness and behavior change increase positive healthy interactions in everyone&#39;s daily life..</h4>
             </div>
+            <div className={style.about_title2}>
+            <h1>About Us</h1>
+            <hr />
+            <p>Linger Coaching uses contemporary behavior change interventions through engaging coaching workshops</p>
+            </div>
+
             <div className={style.about_content}>
                 <div className={style.about_img}>
-                    <img src="/assets/images/lady.jpeg" alt="" />
+                    <img src="/assets/images/new/rania.jpeg" alt="" />
                 </div>
                 <div className={style.about_text}>
-                    <h4>Alicia Hart</h4>
+                    <h4>Rania Abuelzain</h4>
                     <p>Co-Owner</p>
                     <hr />
                     <div className={style.text_title}>
-                        <p>In business, solving issues is about planning, problem solving and implementing systems to improve a business which is a constant work in progress, but beneficial.</p>
-                        <p>For the past 15 years, I’ve run my own business, Simply Organized by Hart, a Professional Organizing business, working with clients one-on-one. I love to organize. As a mother of four (now grown), a wife, and a small business entrepreneur, life has taught me to multitask at home and in business. This helped me develop skills to problem solve and foresee strategies to help simplify and efficiently manage a home, work and life balance.</p>
-                        <p>Within the scope of being a Professional Organizer, I was hired by a high-end estate sales company to help stage homes, work in sales and also help create a back end liquidation process to clear out estates. Through this experience, the next business of Upscale Resale CT was created. A local, online furniture sales website where clients sold their furniture and home decor right from their home in a safe and secure process. I enjoyed learning the process of growing my businesses all while providing customers a service they needed.</p>
-                        <p>I volunteered extensively for 20 years as team manager for several hockey and lacrosse teams and volunteered as a preschool coordinator at our local church. I enjoy puzzles, gardening, photography and history. I am an avid New York Rangers fan and a developing Jacksonville Jaguars fan. I love beach walks, the GTM Reserve and being outdoors with our Siberian Husky, Koda.</p>
-                        <p>I graduated from the University of Connecticut with a B.A. in Human Development and Family Sciences and was a past member of NAPO, National Association of Professional Organizers.</p>
+                        <p>My electrical engineer’s personality traits include curiosity, critical thinking, creativity, effective communication, and a collaborative spirit,  long with my interest in exploring new subjects and interacting with new technology; training and development was my career choice since 1999.</p>
+                        <p>Whereas promoting self-development, helping others, and supporting vocational aspiration, was the motive behind earning my master&#39;s degree in Psychology which helped me implement effective behavior change into training programs and deliver targeted learning interventions.</p>
+                        <p>As a professional certified coach and a certified behavior analyst, I aim to focus on shifting mindset and behavior change coaching.</p>
                     </div>
                 </div>
             </div><div className={style.about_content}>
                 <div className={style.about_img}>
-                    <img src="/assets/images/man.jpeg" alt="" />
+                    <img src="/assets/images/new/tahia.jpeg" alt="" />
                 </div>
                 <div className={style.about_text}>
-                    <h4>Doug Hart</h4>
-                    <p>Co-Owner</p>
+                    <h4>Tahia Abdelrahman</h4>
                     <hr />
                     <div className={style.text_title}>
-                        <p>Doug HartCo-Owner During my career I have experienced many up and down business economic cycles. In some cases these were very disruptive and required the ability to change or pivot. In others these cycles exposed opportunities for those entrepreneurs and businesses organized well enough to seize them. I have had the opportunity to work at many organizational levels with good mentors and have learned the importance of listening, asking questions and also working collaboratively with a wide range of resources. It has made clear to me the importance of regular planning and good communication. This is often the difference between success and failure.</p>
-                        <p>Over the past 35 years I have worked in construction, food services, real estate, manufacturing and private equity. My industry experience includes Automotive, Medical, Industrial, Aerospace, Textile, Transportation, Military, Steel, Testing Instruments and Outdoor Recreational products with a focus on Operations, Planning, New Product Development, Administration and Finance. I was co-owner and COO of an international manufacturer of engineered industrial consumable products; served as COO for a privately owned composite materials manufacturer; was co-owner of a successful business advisory firm focused on driving client sales, income, and overall growth. Most recently I have been an Operating Partner with a successful strategic equity investment group focused on small to midsize businesses.</p>
-                        <p>My personal interests include running, biking, fishing and history. I follow the NFL, College Football and NHL Hockey. I graduated from Hobart & William Smith Colleges.</p>
+                        <p>I have had a wide range of experience in marketing, business, and computer science. I have a bachelor&#39;s degree in information technology, associate degree in software engineering and an associate degree in business.</p>
+                        <p>For the past 13 years, I have been actively involved in my community. I have been serving the low-income, underprivileged, and vulnerable population through the nonprofit that I am a board member of. Through my work with this population, I have been able to see the great need for behavior change and how much potential there is when applied into their lives.</p>
+                        <p>I believe that when behavior is changed positively, it results in productive habits. These habits can be used every day within any interactive setting.</p>
                     </div>
                 </div>
             </div>
