1b0a13a4bf26cbee3c352d95e56b73c6ab2c1142
mubeenhassan <mubeenhassan34@gmail.com>
2022-06-28 14:11:09 +0500

    adding new silk carusal

--- component/pages/services.jsx
+++ component/pages/services.jsx
@@ -1,51 +1,58 @@
-import Link from "next/link";
-import react from "react";
-import Footer from "../footer";
-import style from './../../asserts/css/service.module.css';
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import { Carousel } from "react-responsive-carousel"
-import ServiceBox from "../serviceBox";
-
+import Link from "next/link"
+import react from "react"
+import Footer from "../footer"
+import style from "./../../asserts/css/service.module.css"
+import ServiceBox from "../serviceBox"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 const Services = () => {
-    const SLIDERIMG = [
-        {
-          text: "visit_major_cities",
-          image: "image5.jpeg",
-        },
-        {
-          text: "visit_major_cities",
-          image: "image7.jpeg",
-        },
-        {
-          text: "visit_major_cities",
-          image: "image2.jpeg",
-        }
-        ]
-      
-    return (
-        <div>
-            <Carousel
-                showArrows={true}
-                infiniteLoop={false}
-                showThumbs={false}
-                showStatus={false}
-                autoPlay={true}
-                interval={3000}
-            >
-        {SLIDERIMG.map((item) => (
-           <div className={style.groundsection} style={{ backgroundImage: `url(/assets/images/new/${item.image})`}}>
-         </div>
-        ))}
-    </Carousel>
-          <ServiceBox />
-            <div className={style.servic_box_container}>
-                <div className={style.service_bot_logo}>
-                    <img src="/assets/images/new/image3.jpeg" alt="" />
-                </div>
-            </div>
-            <Footer />
-        </div>
-    )
-};
+  const SLIDERIMG = [
+    {
+      text: "visit_major_cities",
+      image: "image5.jpeg",
+    },
+    {
+      text: "visit_major_cities",
+      image: "image7.jpeg",
+    },
+    {
+      text: "visit_major_cities",
+      image: "image2.jpeg",
+    },
+  ]
 
-export default Services;
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+
+  }
+
+  return (
+    <div>
+      <Slider {...settings}>
+        {SLIDERIMG.map((item) => (
+          <div
+            className={style.groundsection + " " + style.slick_slide}
+            style={{ backgroundImage: `url(assets/images/new/${item.image})` }}
+          >
+            <img src={`/assets/images/new/${item.image}`} alt="" />
+          </div>
+        ))}
+      </Slider>
+      <ServiceBox />
+      <div className={style.servic_box_container}>
+        <div className={style.service_bot_logo}>
+          <img src="/assets/images/new/image3.jpeg" alt="" />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Services
